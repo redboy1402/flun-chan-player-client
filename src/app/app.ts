e8da533b9e584a16c51672b0e5ee83e9b506d313
redboy1402 <101729153+redboy1402@users.noreply.github.com>
2025-07-20 00:42:24 +0800

    GOT A THING!!! WOOOO

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {Socket} from "socket.io";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  inputData: string = '';
+
+  submitData() {
+    if (this.inputData == '') {
+      alert("no input detected")
+    } else {
+      alert(this.inputData)
+    }
+  }
+
   protected readonly title = signal('FlunChanClientPlayer');
 }
